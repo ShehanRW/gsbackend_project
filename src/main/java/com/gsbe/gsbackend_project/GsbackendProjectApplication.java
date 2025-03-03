aaa6d679d7f7ec170cf6d9395d3d35d1c5798110
Shehan wijerathna <shehanranjukaw5@gmail.com>
2025-03-03 19:56:21 +0530

    initial_commit

---
+++ src/main/java/com/gsbe/gsbackend_project/GsbackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gsbe.gsbackend_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsbackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsbackendProjectApplication.class, args);
+	}
+
+}
