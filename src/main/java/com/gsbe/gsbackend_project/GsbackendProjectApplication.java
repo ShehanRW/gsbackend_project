d04c2da43e1f0c874ea7c53c075350354716d9d3
Shehan wijerathna <shehanranjukaw5@gmail.com>
2025-03-04 17:40:29 +0530

    initialize data transfer object

--- src/main/java/com/gsbe/gsbackend_project/GsbackendProjectApplication.java
+++ src/main/java/com/gsbe/gsbackend_project/GsbackendProjectApplication.java
@@ -1,7 +1,9 @@
 package com.gsbe.gsbackend_project;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GsbackendProjectApplication {
@@ -10,4 +12,8 @@ public class GsbackendProjectApplication {
 		SpringApplication.run(GsbackendProjectApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
--- src/main/java/com/gsbe/gsbackend_project/dto/UserDTO.java
+++ src/main/java/com/gsbe/gsbackend_project/dto/UserDTO.java
@@ -1,5 +1,15 @@
 package com.gsbe.gsbackend_project.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
-    
+    private int ID;
+    private String userName;
+    private String dateOfBirth;
+    private String address;
 }
