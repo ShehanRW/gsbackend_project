c6ea6193474abc5f447cd5462544a236acab4298
Shehan wijerathna <shehanranjukaw5@gmail.com>
2025-03-04 19:30:29 +0530

    defie user model

--- src/main/java/com/gsbe/gsbackend_project/model/UserModel.java
+++ src/main/java/com/gsbe/gsbackend_project/model/UserModel.java
@@ -1,5 +1,20 @@
 package com.gsbe.gsbackend_project.model;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class UserModel {
-    
+    @Id
+    private int ID;
+    private String userName;
+    private String dateOfBirth;
+    private String address;
 }
